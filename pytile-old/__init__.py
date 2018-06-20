0be2befc9a056f7dd9ffcb960d7fddc94fc5a6c4
Aaron Bach <bachya1208@gmail.com>
2018-06-20 16:55:51 -0600

    Removed old files

--- pytile-old/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Define module-level imports."""
-
-# pylint: disable=wildcard-import
-
-from .__version__ import __version__  # noqa
-from .client import Client  # noqa
--- pytile-old/__version__.py
+++
@@ -1,3 +0,0 @@
-"""Define a version constant."""
-
-__version__ = '1.1.0'
