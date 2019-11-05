63985237bddfe147c5d9f60c033de2cca851de0f
Aaron Bach <bachya1208@gmail.com>
2019-11-05 11:05:09 -0700

    Store UUIDs as strings (#19)

--- pytile/tile.py
+++ pytile/tile.py
@@ -1,6 +1,5 @@
 """Define endpoints for interacting with Tiles."""
 from typing import Awaitable, Callable, List, Optional
-from uuid import UUID
 
 
 class Tile:  # pylint: disable=too-few-public-methods
@@ -10,18 +9,18 @@ class Tile:  # pylint: disable=too-few-public-methods
         self,
         request: Callable[..., Awaitable[dict]],
         *,
-        user_uuid: Optional[UUID] = None,
+        user_uuid: Optional[str] = None,
     ) -> None:
         """Initialize."""
         self._request: Callable[..., Awaitable[dict]] = request
-        self._user_uuid: Optional[UUID] = user_uuid
+        self._user_uuid: Optional[str] = user_uuid
 
     async def all(self, whitelist: list = None, show_inactive: bool = False) -> list:
         """Get all Tiles for a user's account."""
         list_data: dict = await self._request(
             "get", f"users/{self._user_uuid}/user_tiles"
         )
-        tile_uuid_list: List[UUID] = [
+        tile_uuid_list: List[str] = [
             tile["tile_uuid"]
             for tile in list_data["result"]
             if not whitelist or tile["tileType"] in whitelist
